67ac2d5cf08fbc5158671c71e625c19109be87ef
Malo10LeGoff <malolegoff@gmail.com>
2021-09-19 22:51:53 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-205e4733a0908bafc1b8.js"
+    "url": "webpack-runtime-3d630d6ae94947e8920d.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10335bc2563ec838a2ab7dd2b04d7176"
+    "revision": "f81f0b0175b16a77a799a259219dc65d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "a56f167f9c2c0ec912f0484b5182c012"
+    "revision": "cee26b317980e33628e844ee1d7b7d98"
   },
   {
     "url": "polyfill-ed717a4fd71e6ab9d4db.js"
